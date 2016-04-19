6246e400eab3ab5233047a356b61ef00e717d7fe
Nicolas HUMBERT <nicolas.humbert@live.fr>
2016-04-18 17:05:28 -0700

    Update orderby.ts

--- orderby.ts
+++ orderby.ts
@@ -5,13 +5,25 @@ import { Pipe,PipeTransform } from "angular2/core";
 })
 export class OrderByPipe implements PipeTransform {
   transform(array:Array<any>, args?) {
+    // Check if array exists, in this case array contains articles and args is an array that has 1 element : !id
     if(array) {
+      // get the first element
       let orderByValue = args[0]
+      let byVal = 1
+      // check if exclamation point 
+      if(orderByValue.charAt(0) == "!") {
+        // reverse the array
+        byVal = -1
+        orderByValue = orderByValue.substring(1)
+      }
+      console.log("byVal",byVal);
+      console.log("orderByValue",orderByValue);
+
       array.sort((a: any, b: any) => {
-        if(a[orderByValue] > b[orderByValue]) {
-          return -1;
-        } else if (a[orderByValue] < b[orderByValue]) {
-          return 1;
+        if(a[orderByValue] < b[orderByValue]) {
+          return -1*byVal;
+        } else if (a[orderByValue] > b[orderByValue]) {
+          return 1*byVal;
         } else {
           return 0;
         }
