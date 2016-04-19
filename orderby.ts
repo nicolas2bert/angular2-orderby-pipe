d6e397ca056e58bed2fd59b5ac69be038021927e
Nicolas HUMBERT <nicolas.humbert@live.fr>
2016-04-18 17:15:07 -0700

    Update orderby.ts

--- orderby.ts
+++ orderby.ts
@@ -1,6 +1,8 @@
+//The pipe class implements the PipeTransform interface's transform method that accepts an input value and an optional array of parameters and returns the transformed value.
 import { Pipe,PipeTransform } from "angular2/core";
-
+//We tell Angular that this is a pipe by applying the @Pipe decorator which we import from the core Angular library.
 @Pipe({
+  //The @Pipe decorator takes an object with a name property whose value is the pipe name that we'll use within a template expression. It must be a valid JavaScript identifier. Our pipe's name is orderby.
   name: "orderby"
 })
 export class OrderByPipe implements PipeTransform {
